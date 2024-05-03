fe45c46e8bacf4c2e359a2f313cee31cd580010f
Le Hong Duc <hongducthbk123@gmail.com>
2024-05-03 12:21:08 +0700

    twolip: Build Aperture

--- device.mk
+++ device.mk
@@ -139,6 +139,7 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
+    Aperture \
     android.frameworks.sensorservice@1.0.vendor \
     android.hardware.camera.device@3.5:64 \
     android.hardware.camera.provider@2.4-impl:32 \
